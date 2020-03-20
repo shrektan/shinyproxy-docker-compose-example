bbb495f3fef2019bd016fb597ad13e6549766102
shrektan <shrektan@126.com>
2020-03-20 20:53:50 +0800

    tweak the example app

--- apps/test/server.R
+++ apps/test/server.R
@@ -1,3 +1,7 @@
 function(input, output, session) {
   output$tbl <- DT::renderDT(iris, server = TRUE)
+  output$print <- renderPrint({
+    print("Selected rows:")
+    input$tbl_rows_selected
+  })
 }
--- apps/test/ui.R
+++ apps/test/ui.R
@@ -1,4 +1,5 @@
 library(shiny)
 fluidPage(
+  verbatimTextOutput("print"),
   DT::DTOutput('tbl')
 )
